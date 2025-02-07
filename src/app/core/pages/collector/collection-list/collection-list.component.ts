36010aae6f223fde56c4cded58f72079c60c4146
asmaabarj <asmaabarj5@gmail.com>
2025-02-07 15:41:00 +0100

    ajouter un composant de collecteur

---
+++ src/app/core/pages/collector/collection-list/collection-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './collection-list.component.html',
+})
+export class CollectionListComponent {
+
+}
